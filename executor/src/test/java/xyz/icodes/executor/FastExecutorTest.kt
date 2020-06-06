0825279a472f84c1cc941567d13a59e23bca9500
zhengjianhui <zhengjianhui@gmail.com>
2020-06-06 17:04:28 +0800

    move file

---
+++ executor/src/test/java/xyz/icodes/executor/FastExecutorTest.kt
@@ -0,0 +1,50 @@
+package xyz.icodes.executor
+
+import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingDeque
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicInteger
+
+class FastExecutorTest {
+    fun run() {
+        val executor = FastExecutor.newExecutor(
+            name = "Demo",
+            corePoolSize = 1,
+            maximumPoolSize = 2,
+            keepAliveTime = 100,
+            unit = TimeUnit.MILLISECONDS,
+            workQueue =  LinkedBlockingDeque(2)
+        )
+
+        val counter = AtomicInteger(0)
+
+        (0..10).forEach {
+            executor.execute(object : Runnable {
+                override fun run() {
+                    val time = System.nanoTime() % 20
+                    println("${Thread.currentThread()} sleep $time ms, ${toString()}")
+                    Thread.sleep(time)
+                    println("${Thread.currentThread()}, ${toString()}, finished")
+
+
+                    if (counter.incrementAndGet() == 11) {
+                        println("executor: $executor")
+
+                        val handler = Executors.newScheduledThreadPool(1)
+                        handler.schedule({
+                            println("executor: $executor")
+                        }, 0, TimeUnit.MILLISECONDS)
+
+                        handler.schedule({
+                            println("executor: $executor")
+                        }, 2000, TimeUnit.MILLISECONDS)
+                    }
+                }
+
+                override fun toString(): String {
+                    return "task-$it"
+                }
+            })
+        }
+    }
+}
